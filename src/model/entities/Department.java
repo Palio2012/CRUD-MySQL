ee7c03f65960ce1bce3e31ae2e431fefa5ffb6e5
Palio2012 <daniasrayan.com@gmail.com>
2022-10-10 14:46:01 -0300

    Department entity class

---
+++ src/model/entities/Department.java
@@ -0,0 +1,57 @@
+package model.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String name;
+
+    public Department () {
+    }
+
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department that)) return false;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getName(), that.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
